990e5406e04b7ba1cf2b6dad790f5c4dc5b768f2
Gabriel Fernandes Bispo <gabrielfbispo88@gmail.com>
2025-02-15 12:48:52 -0300

    ajuste no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,9 +1,9 @@
-// Fetch ou Axios
 import "dotenv/config";
 import axios from "axios";
 
-const { NODE_ENV } = process.env.NODE_ENV;
-const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+// const { NODE_ENV } = process.env.NODE_ENV;
+// const URL = NODE_ENV === 'development' ? "http://localhost:3001/api" : "/api";
+const URL = "https://spotify-project-2isf.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
