96d2e86b77047d7908cb6177b642552838835877
Gabriel Fernandes Bispo <160673298+GabrielBispo1@users.noreply.github.com>
2025-02-20 21:42:48 -0300

    removendo uri connect.js

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const URI = "mongodb+srv://gabrielbispo1:aj7xJpeamAl5eZjq@cluster0.qz9rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const URI = "mongodb+srv://@cluster0.qz9rd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
